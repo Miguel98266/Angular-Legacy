d03f8909ed934c023b37d157c3c1b5d2d23e6547
Miguel Calixto <miguel98266@gmail.com>
2025-08-10 22:58:28 -0600

    Agregar seccion heroes con detalles

--- 02-spa/src/app/components/heroes/heroes.component.ts
+++ 02-spa/src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService ,Heroe } from '../../servicios/heroes.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-heroes',
@@ -9,11 +10,17 @@ import { HeroesService ,Heroe } from '../../servicios/heroes.service';
 })
 export class HeroesComponent implements OnInit {
   heroes:Heroe[] = [];
-  constructor(private _heroesService:HeroesService) { }
+  constructor(private _heroesService:HeroesService,
+              private router:Router
+  ) { }
 
   ngOnInit(): void {
     this.heroes = this._heroesService.getHeroes();
-    console.log(this.heroes);
-  }
+    // console.log(this.heroes);
 
+  }
+  verHeroe(idx:number) {
+    this.router.navigate(['/heroe', idx]);
+    this._heroesService.getHeroes()[idx]; // This line is not necessary, but it shows how to access a hero by index 
+  }
 }
