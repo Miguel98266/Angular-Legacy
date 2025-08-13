684e5ad50cd243242b61a5058394c31053f6baab
Miguel Calixto <miguel98266@gmail.com>
2025-08-12 23:41:18 -0600

    pipe slice

--- 03-pipes/src/app/app.component.ts
+++ 03-pipes/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  nombre: string = 'Capitán América';
+  nombre: string = 'Miguel Angel';
+  arreglo=[1,2,3,4,5,6,7,8,9,10];
 }
