3cc61ddef39f9e33dd18c4a57b9a83bd28d64bd7
Miguel Calixto <miguel98266@gmail.com>
2025-08-18 21:07:58 -0600

    pipe idiomas

--- 03-pipes/src/app/app.module.ts
+++ 03-pipes/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule , LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { registerLocaleData } from '@angular/common';
 import localeEs from '@angular/common/locales/es';
+import localeFr from '@angular/common/locales/fr';
 import { AppComponent } from './app.component';
 
 registerLocaleData(localeEs, 'es'); // Register Spanish locale
-
+registerLocaleData(localeFr, 'fr');
 @NgModule({
   declarations: [
     AppComponent
