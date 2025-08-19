48b52f974f37268ef36a5121d62ff02235e7d029
Miguel Calixto <miguel98266@gmail.com>
2025-08-18 19:47:28 -0600

    json pipe

--- 03-pipes/src/app/app.component.ts
+++ 03-pipes/src/app/app.component.ts
@@ -7,5 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   nombre: string = 'Miguel Angel';
-  arreglo=[1,2,3,4,5,6,7,8,9,10];
+  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  PI: number = Math.PI;
+  porcentaje: number = 0.234;
+  salario: number = 1234.5;
+
+  valorPromesa = new Promise<string>((resolve, reject) => {
+    setTimeout(() => {
+      resolve('¡Llegó la data!');
+    }, 3500);
+  });
+
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 500,
+    direccion: {
+      calle: 'Primera',
+      numero: 20
+    }
+  }
 }
